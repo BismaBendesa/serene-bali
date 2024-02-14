dabdeb884b62194fb94e95b51c88236e125be993
Bisma Bendesa <bismabendesajaya@gmail.com>
2024-02-14 15:09:15 +0800

    Add search section

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -9,9 +9,9 @@
   @vite('resources/css/app.css')
   <title>Landing Page</title>
 </head>
-<body class="font-poppins max-w-screen-2xl m-auto">
+<body class="font-poppins m-auto">
   {{-- start header --}}
-  <header class="flex items-center justify-between w-full md:px-16 md:py-5 py-4 px-8 fixed z-10 bg-white md:left-0 md:right-0 m-auto max-w-[1440px]">
+  <header class="flex items-center justify-between w-full md:px-16 md:py-5 py-4 px-8 fixed z-50 bg-white md:left-0 md:right-0 m-auto max-w-[1920px] ">
     <div class="logo flex items-center md:gap-4 gap-2">
       <img src="{{URL('storage/serene-bali-logo-black.png')}}" alt="Serene Bali Logo" class="md:w-[19px] w-[14px]">
       <a href="" class="md:text-base text-sm"><span class="font-bold">S E R E N E</span> <span class="font-light">Bali</span></a>
@@ -28,8 +28,8 @@
     <button class="border border-black rounded-3xl px-6 py-2 font-medium text-base hover:bg-black hover:text-white transition-colors delay-50 md:block hidden">Sign Up</button> 
     <div class="block md:hidden cursor-pointer" id="burgerButton">
       <svg width="32" height="16" viewBox="-10 0 32 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect width="32" height="4" rx="2" fill="black" id="rect-top" class="transition duration-150"/>
-        <rect y="12" width="32" height="4" rx="2" fill="black" id="rect-bottom" class="transition duration-150"/>
+        <rect width="32" height="4" rx="2" fill="black" id="rect-top" class="transition duration-500"/>
+        <rect y="12" width="32" height="4" rx="2" fill="black" id="rect-bottom" class="transition duration-500"/>
         {{-- -translate-y-1 rotate-45  --}}
         {{-- translate-y-1.5 -translate-x-2 -rotate-45  --}}
         {{-- viewBox="-5 0 32 16" --}}
@@ -37,7 +37,7 @@
     </div>
   </header>
   {{-- mobile nav --}}
-  <nav class="flex md:hidden gap-4 flex-col absolute left-0 right-0 py-4 px-8 bg-gray-200 transition -translate-y-72 duration-500 z-0 top-14" id="mobileMenu">
+  <nav class="flex md:hidden gap-4 flex-col absolute left-0 right-0 py-4 px-8 bg-gray-200 transition -translate-y-72 duration-500 z-10 top-14" id="mobileMenu">
     <a href="" class="text-base active:font-bold focus:font-bold focus:underline focus:underline-offset-2 transition-all delay-50 h-9">Buy</a>
     <a href="" class="text-base active:font-bold focus:font-bold focus:underline focus:underline-offset-2 transition-all delay-50 h-9">Rent</a>
     <a href="" class="text-base active:font-bold focus:font-bold focus:underline focus:underline-offset-2 transition-all delay-50 h-9">Consultation</a>
@@ -47,42 +47,43 @@
   {{-- end header --}}
 
   {{-- start search-section --}}
-  <div class="search-section md:pt-20 md:px-16 md:py-5 py-4 px-8">
-    <h1>Buy & Rent Villa Properties With Serene Bali</h1>
-    <p>We are property agency startup that provides best service.</p>
+  <div class="search-section pt-20 px-8 max-h-[320px] md:py-5 md:pt-20 md:max-h-[450px] bg-no-repeat bg-cover w-full max-w-[1920px] m-auto" style="background-image:url({{asset('storage/hero-image.png')}})">
+    <h1 class="text-white font-bold text-center m-auto text-xl md:text-5xl md:mt-16 md:max-w-[850px] md:leading-normal ">Buy & Rent Villa Properties With Serene Bali</h1>
+    <p class="text-white text-center text-xs m-auto mt-2 md:text-xl md:mt-6">We are property agency startup that provides best service.</p>
     {{-- we need to make this search bar as a component maybe --}}
-    <div class="search-bar">
-      <form action="">
-        <div class="input-form">
-          <label for="action">Action</label>
-          <select name="action" id="action">
-            <option value="Buy">Buy</option>
-            <option value="Rent">Rent</option>
-            <option value="Rent">Sell</option>
+    <div class="search-bar bg-[#EAE6E3] max-w-[1060px] px-5 py-5 mt-8 rounded-lg md:px-8 md:py-8 md:rounded  mx-auto md:mt-16">
+      <form action="" class="flex flex-col justify-center gap-3 md:gap-6 md:flex-row  md:items-end">
+        <div class="input-form flex flex-col gap-2 relative after:content-['▼'] after:right-4 after:top-9 text-sm md:after:text-base md:after:top-12 md:after:right-3 after:absolute z-0">
+          <label for="action" class="text-[#5F5E5E] font-medium text-sm md:text-base md:text-black">Action</label>
+          <select name="action" id="action" class="bg-transparent border border-black appearance-none cursor-pointer rounded-lg py-2 px-4 md:rounded-xl  md:px-3 md:py-3 md:min-w-[188px] text-sm md:text-base">
+            <option value="Buy" class="text-sm md:text-base">Buy</option>
+            <option value="Rent" class="text-sm md:text-base">Rent</option>
+            <option value="Sell" class="text-sm md:text-base">Sell</option>
           </select>
         </div>
-        <div class="input-form">
-          <label for="type">Type</label>
-          <select name="type" id="type">
+        <div class="input-form flex flex-col gap-2 relative after:content-['▼'] after:right-4 after:top-9 text-sm md:after:text-base md:after:top-12 md:after:right-3 after:absolute z-0">
+          <label for="type" class="text-[#5F5E5E] font-medium text-sm md:text-base md:text-black">Type</label>
+          <select name="type" id="type" class="bg-transparent border border-black appearance-none cursor-pointer rounded-lg py-2 px-4 md:rounded-xl  md:px-3 md:py-3 md:min-w-[188px] text-sm md:text-base">
             <option value="Villa">Villa</option>
             <option value="Land">Land</option>
             <option value="Homestay">Homestay</option>
           </select>
         </div>
-        <div class="input-form">
-          <label for="location">Location</label>
-          <input type="text" id="location" name="location">
+        <div class="input-form flex flex-col gap-2 relative z-0">
+          <label for="location" class="text-[#5F5E5E] font-medium text-sm md:text-base md:text-black">Location</label>
+          <input type="text" id="location" name="location" class="bg-transparent border border-black appearance-none rounded-lg py-2 px-4 md:rounded-xl  md:px-3 md:py-3 md:min-w-[188px] text-sm md:text-base" value="Ubud">
         </div>
-        <div class="input-form">
-          <label for="location">Budget</label>
-          <select name="location" id="location">
+        <div class="input-form flex flex-col gap-2 relative after:content-['▼'] after:right-4 after:top-9 text-sm md:after:text-base md:after:top-12 md:after:right-3 after:absolute z-0">
+          <label for="location" class="text-[#5F5E5E] font-medium text-sm md:text-base md:text-black">Budget</label>
+          <select name="location" id="location" class="bg-transparent border border-black appearance-none cursor-pointer rounded-lg py-2 px-4 md:rounded-xl  md:px-3 md:py-3 md:min-w-[188px] text-sm md:text-base">
             <option value="<$1000">Less $1000</option>
             <option value="$1000-$2000">$1000 - $2000</option>
             <option value="$2000-5000">$2000 - $5000</option>
             <option value=">$5000">Over $5000</option>
           </select>
         </div>
-        <button type="submit">Search <span class="material-symbols-rounded">
+        {{-- might need to add component to the button --}}
+        <button type="submit" class="flex items-center justify-center bg-[#182955] text-white gap-2 px-4 py-3 max-h-12 rounded-lg mt-3 text-sm md:text-base">Search <span class="material-symbols-rounded text-white md:mt-0 text-sm md:text-base">
           search
           </span>
         </button>
