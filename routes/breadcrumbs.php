dd9e5c2bda08a6add2af7cd3cb628552bcc15814
Bisma Bendesa <bismabendesajaya@gmail.com>
2024-09-11 10:25:59 +0800

    Add breadcrumbs to routes

---
+++ routes/breadcrumbs.php
@@ -0,0 +1,18 @@
+<?php // routes/breadcrumbs.php
+
+// Note: Laravel will automatically resolve `Breadcrumbs::` without
+// this import. This is nice for IDE syntax and refactoring.
+use Diglactic\Breadcrumbs\Breadcrumbs;
+
+// This import is also not required, and you could replace `BreadcrumbTrail $trail`
+//  with `$trail`. This is nice for IDE type checking and completion.
+use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+
+Breadcrumbs::for('user-dashboard', function (BreadcrumbTrail $trail) {
+  $trail->push('Dashboard', route('user-dashboard'));
+});
+
+// Breadcrumbs::for('blog', function (BreadcrumbTrail $trail) {
+//   $trail->parent('user-dashboard');
+//   $trail->push('Blog', route('landing-page'));
+// });
