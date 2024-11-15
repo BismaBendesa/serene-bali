a1475bb629364b0f5960d1a6c16b32111743a738
Bisma Bendesa <bismabendesajaya@gmail.com>
2024-11-15 09:09:39 +0800

    routing

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\User\LoginController;
 use App\Http\Controllers\User\RegisterController;
 use App\Http\Controllers\User\DashboardController;
+use App\Http\Controllers\User\PropertyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -32,4 +33,10 @@ Route::get('/admin/dashboard', function () {
     ]);
 })->middleware('admin', 'auth');
 
+// user routes
 Route::get('/user/dashboard', [DashboardController::class, 'index'])->name('user-dashboard')->middleware('user', 'auth');
+Route::get('/user/property', [PropertyController::class, 'index'])->name('property-catalog')->middleware('user', 'auth');
+Route::get('/user/property/search', [PropertyController::class, 'search'])->name('property-search')->middleware('user', 'auth');
+
+
+Route::get('/user/property/{id}', [PropertyController::class, 'show'])->name('property-details')->middleware('user', 'auth');
